6e4936cb1a31c03743b316c1503e8a22e7100e94
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2022-10-31 14:09:12 -0300

    chore: created header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import './global.css'
 
+import { Header } from './components/Header'
+
 export function App() {
-  return <h1>Ignite Feed</h1>
+  return (
+    <div>
+      <Header />
+    </div>
+  )
 }
