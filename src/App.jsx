1f799af136c1caea9888d0357088e6e2b090e750
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2022-10-31 14:57:25 -0300

    chore: created wrapper element that contains aside & main components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import './global.css'
 
+import styles from './App.module.css'
+
 import { Header } from './components/Header'
 
 export function App() {
   return (
     <div>
       <Header />
+      <div className={styles.wrapper}>
+        <aside></aside>
+        <main></main>
+      </div>
     </div>
   )
 }
